8a1cfcfd35cd1e925c80fd94ce019e63e5057563
laiyungseng <laiyungseng@hotmail.com>
2026-01-18 14:55:24 +0800

    Delete Documents/GitHub/brew/SQL AIAGENT directory

--- Documents/GitHub/brew/SQL AIAGENT/basemodel.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-
-class DBConfig(BaseModel):
-    host: str = Field(default='127.0.0.1', description='Database host address', examples='127.0.0.1')
-    port: int = Field(..., description='Port Number in database config', examples=3306, ge=1, le=65535)
-    user: str = Field(..., description='username in databasee', examples='root', min_length=1)
-    password: str = Field(..., description='password set in database', min_length=1)
-    database: str = Field(..., description='tables name', min_length=1)
